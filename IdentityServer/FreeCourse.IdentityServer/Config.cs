e42ff94391094d10fd11bfeed650db04f7286604
BatuhanBedir <114083704+BatuhanBedir@users.noreply.github.com>
2023-08-29 16:51:51 +0300

    IdentityServer

---
+++ IdentityServer/FreeCourse.IdentityServer/Config.cs
@@ -0,0 +1,68 @@
+﻿// Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
+
+
+using IdentityServer4;
+using IdentityServer4.Models;
+using System;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+
+namespace FreeCourse.IdentityServer
+{
+    public static class Config
+    {
+        public static IEnumerable<ApiResource> ApiResources =>
+            new ApiResource[]
+            {                       //bu resource'nın izli       bu
+                 new ApiResource("resource_catalog"){Scopes={"catalog_fullpermission"}},
+               new ApiResource("resource_photo_stock"){Scopes={"photo_stock_fullpermission"}},
+                new ApiResource(IdentityServerConstants.LocalApi.ScopeName)
+            };
+
+        public static IEnumerable<IdentityResource> IdentityResources =>
+                   new IdentityResource[]
+                   {
+                       //kullanıcı hakkında hangi bilgilere clientten erişilebilir işlemi
+                       new IdentityResources.Email(),//hazır claim
+                       new IdentityResources.OpenId(), //jwt almak isteniliyorsa token içerisinde sub(subject) keyword'ün jwt payload'ındaki sub keywordünün dolu olması gerekmektedir.
+                       new IdentityResources.Profile(),
+                       new IdentityResource(){Name="roles", DisplayName="Roles", Description="Kullanıcı rolleri", UserClaims = new[]{"role"} }//hangi claimle maplenecek.
+
+                   };
+
+        public static IEnumerable<ApiScope> ApiScopes =>
+            new ApiScope[]
+            {
+                new ApiScope("catalog_fullpermission","Catalog API için full erişim"),
+                new ApiScope("photo_stock_fullpermission","Photo Stock API için full erişim"),
+                new ApiScope(IdentityServerConstants.LocalApi.ScopeName)
+            };
+
+        public static IEnumerable<Client> Clients =>
+            new Client[]
+            {
+                new Client
+                {
+                    ClientName = "Asp.Net Core MVC",
+                    ClientId = "WebMvcClient",
+                    ClientSecrets={new Secret("secret".Sha256())},
+                    AllowedGrantTypes = GrantTypes.ClientCredentials, //refresh token yok
+                    AllowedScopes = { "catalog_fullpermission", "photo_stock_fullpermission", IdentityServerConstants.LocalApi.ScopeName }
+                },
+                new Client
+                {
+                    ClientName = "Asp.Net Core MVC",
+                    ClientId = "WebMvcClientForUser",
+                    AllowOfflineAccess = true, //OfflineAccess 'ı kullanabilmek icin izin
+                    ClientSecrets={new Secret("secret".Sha256())},
+                    AllowedGrantTypes = GrantTypes.ResourceOwnerPassword,
+                    AllowedScopes = {IdentityServerConstants.StandardScopes.Email, IdentityServerConstants.StandardScopes.OpenId, IdentityServerConstants.StandardScopes.Profile, IdentityServerConstants.LocalApi.ScopeName, IdentityServerConstants.StandardScopes.OfflineAccess,"roles"},//refresh. kullanıcı offline olduğunda bile token-kullanıcı adına refresh tokenla yeni token alabilme.
+                    AccessTokenLifetime=1*60*60,
+                    RefreshTokenExpiration = TokenExpiration.Absolute,
+                    AbsoluteRefreshTokenLifetime = (int)(DateTime.Now.AddDays(60)-DateTime.Now).TotalSeconds, //refresh token ömrü- Token alırken refresh token de alacak.
+                    RefreshTokenUsage = TokenUsage.ReUse //tekrar kullanılabilir.
+                }
+            };
+    }
+}
--- Services/Catalog/FreeCourse.Services.Catalog/Startup.cs
+++ Services/Catalog/FreeCourse.Services.Catalog/Startup.cs
@@ -1,8 +1,10 @@
 using FreeCourse.Services.Catalog.Services.CategoryServices;
 using FreeCourse.Services.Catalog.Services.CourseService;
 using FreeCourse.Services.Catalog.Settings;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Authorization;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -24,11 +26,22 @@ namespace FreeCourse.Services.Catalog
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(opt =>
+            {
+                //tokený kimin daðýttýgýnýn bilgisi
+                opt.Authority = Configuration["IdentityServerURL"]; //public key.
+                opt.Audience = "resource_catalog"; //gelen tokenýn audience kontrol
+                opt.RequireHttpsMetadata = false;
+
+            });
             services.AddScoped<ICategoryService, CategoryService>();
             services.AddScoped<ICourseService, CourseService>();
 
             services.AddAutoMapper(typeof(Startup));
-            services.AddControllers();
+            services.AddControllers(opt =>
+            {
+                opt.Filters.Add(new AuthorizeFilter());
+            });
 
             services.Configure<DatabaseSettings>(Configuration.GetSection("DatabaseSettings"));
 
@@ -55,6 +68,7 @@ namespace FreeCourse.Services.Catalog
 
             app.UseRouting();
 
+            app.UseAuthentication();
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
